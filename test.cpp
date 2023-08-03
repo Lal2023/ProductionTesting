8751adffe063cb29f0e35368f483d357177df69d
Rowdy2018 <117493382+Rowdy2018@users.noreply.github.com>
2023-08-03 16:36:20 +1000

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+template<class T>
+void myFunction(T arg){
+    cout  << arg << endl;
+}
+
+
+int main(){
+
+    cout << "Calling the function from within the main" << endl;
+
+    myFunction("Hi");
+  
+}
